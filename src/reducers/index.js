5eb459ae80374c3d356b5522f98188bc6a91af2e
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-03-31 17:37:05 -0400

    feat(redux): add redux framing to project

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({
+//   // enter all app reducers here
+// });
+
+// export default rootReducer;
+
+export default dummyFunc = (a,b) => {
+  return a+b;
+}

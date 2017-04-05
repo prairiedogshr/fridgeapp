5c3ffb97c28d9eb359d61241f90fda310edb6991
Kyle Greene <ksgreene1906@hotmail.com>
2017-04-05 16:09:23 -0400

    typos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,7 +10,7 @@ import userReducer from './user';
 import authReducer from './auth';
 import tasksReducer from './tasks';
 import settingsReducer from './settings';
-import userReducer from './user';
+
 
 const rootReducer = combineReducers({
   homelessReducer,
@@ -23,4 +23,3 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
-ucer;

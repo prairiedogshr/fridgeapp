ef16acc06ea74965cf78c258c8c1b7f19002f271
dhpdx <danhendrix@gmail.com>
2017-04-06 10:32:50 -0400

    feat(house profile): add start for house profile container and component

--- src/containers/houseContainer.jsx
+++ src/containers/houseContainer.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react'
 import { connect } from 'react-redux';
 import { updateHouseInfo, removeUser, addUser } from '../actions';
 import User from '../components/user.jsx';
+import HouseInfo from '../components/houseInfo.jsx';
 
 class House extends Component {
 
@@ -19,15 +20,25 @@ class House extends Component {
 	}
 
 	render() {
-		const users = this.props.users;
+		const users = this.props.house.users;
+		console.log('users! ', users)
 		const houseInfo = this.props.houseInfo;
 
 		return (
-			<div>
-				<p onClick={() => {
-					this.updateHouse('address', 'here')
-				}}>{this.props.house.info.address}</p>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<HouseInfo info={this.props.house.info} onClick={() => {
+							this.updateHouse('address', 'here')
+						}} />
+					</div>
+					<div className="col-md-6">
+						{this.props.house.users.map(user => 
+							<p key={user}>{user}</p>
+						)}
+				</div>
 			</div>
+		</div>
 		)
 	}
 }

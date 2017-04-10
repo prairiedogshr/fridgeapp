355b9d221e49940333918dd71373cba2b0af9f7e
Kyle Greene <ksgreene1906@hotmail.com>
2017-04-10 13:44:12 -0400

    small fixes

--- src/components/pageNotFound.jsx
+++ src/components/pageNotFound.jsx
@@ -3,12 +3,12 @@ import React, {Component} from 'react';
 class NotAvaiable extends Component{
 
   render(){
-    return(){
+    return({
       <div>
         <h1> 404- Page Not Found</h1>
         <p> I'm sorry, the page you are looking for isn't available.</p>
       </div>
-    }
+    })
   }
 }
 

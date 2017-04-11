698322e2dfd682ec64532c962a3e80ddbd8b1123
Jarett Engdahl <jaretten@gmail.com>
2017-04-11 15:45:21 -0400

    rebasing

---
+++ src/containers/expensesContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class houseExpenses extends Component {
+
+  render() {
+    return (
+      <div>Ayyyyo</div>
+    );
+  }
+}
+
+export default connect()(houseExpenses);

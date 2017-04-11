c971c26eddfaec08e6ab4dd03a0d30fe866b048b
Jarett Engdahl <jaretten@gmail.com>
2017-04-11 19:52:03 -0400

    budget page

--- src/containers/expensesContainer.jsx
+++ src/containers/expensesContainer.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ExpenseMonth from '../components/expenses/currentMonth.jsx';
+import CurrentMonthExpense from '../components/expenses/currentMonthExpenses.jsx';
 
 class houseExpenses extends Component {
-
   render() {
+    console.log(this.props.house)
     return (
-      <div>Ayyyyo</div>
+      <div>
+        <ExpenseMonth />
+        <CurrentMonthExpense id={this.props.house.house_id}/>
+      </div>
     );
   }
 }
 
-export default connect()(houseExpenses);
+const mapStateToProps = ({ houseReducer }) => ({
+  house: houseReducer
+});
+
+export default connect(
+  mapStateToProps,
+)(houseExpenses);

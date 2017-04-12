02e2e3b368328ba74a474dff384befb79f6c2b6c
Jarett Engdahl <jaretten@gmail.com>
2017-04-12 15:15:25 -0400

    login work

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -8,35 +8,46 @@ import {  loginUser,
   registerUser,
   protectedTest} from '../actions/auth/auth'
 
-
 class Auth extends Component{
   constructor(props){
     super(props);
     this.state={
-      signup: false,
-      login: true,
+      signup:false,
+      login:true
     }
   }
 
-  switch = (word) => {
-    return this.setState({login: !this.state.login, signup: !this.state.signup})
+  componentWillMount() {
+    console.log("CWM", logoutUser)
+    logoutUser()
+  }
+
+  switch = (word) =>{
+    var signup;
+    var login;
+    if(word === "signup"){signup = true; login = false;}
+    else{login = true; signup= false;}
+    return this.setState({login:login, signup:signup})
   }
 
   render(){
-    let word = this.state.signup ? "login" : "signup"
+    var self = this;
     return(
       <div>
-        {this.state.signup ? <Register/> : null}
-        {this.state.login ? <Log /> : null}
         <div id= "buttons">
-          <p id= "loginButton" onClick = {this.switch}>{word}</p>
+          <p id ="signupButton" onClick ={self.switch.bind(null,"signup")}
+            className={self.state.signup ? "yellow": "blue"}> Sign Up </p>
+          <p id= "loginButton" onClick = {self.switch.bind(null, "login")}
+            className={self.state.login ? "yellow": "blue"}>Login</p>
         </div>
+        {self.state.signup?<Register/> : null}
+        {self.state.login? <Log /> : null}
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ authReducer }) => ({
+const mapStateToProps = ({authReducer}) => ({
   auth: authReducer
 })
 

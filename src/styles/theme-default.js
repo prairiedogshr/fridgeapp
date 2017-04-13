b7209a08a9ae0da39b85262ffa64871eb68a2a7b
Richard A. Serdici <rserdici@gmail.com>
2017-04-13 16:13:54 -0400

    feat(ui): Material Design

--- src/styles/theme-default.js
+++ src/styles/theme-default.js
@@ -1,19 +1,19 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { blue600, grey900 } from 'material-ui/styles/colors';
+import { cyan400, grey900 } from 'material-ui/styles/colors';
 
 const themeDefault = getMuiTheme({
   palette: {
   },
   appBar: {
     height: 57,
-    color: blue600,
+    color: cyan400,
   },
   drawer: {
     width: 230,
     color: grey900,
   },
   raisedButton: {
-    primaryColor: blue600,
+    primaryColor: cyan400,
   },
 });
 

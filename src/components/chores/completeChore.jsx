e20f13e8d05ade79e7e37c09edd659942ce169cd
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-04-13 15:22:33 -0400

    fix(login): fix chores and tasks dashboard load when logging in

--- src/components/chores/completeChore.jsx
+++ src/components/chores/completeChore.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function CompletedChore(props) {
   const complete = props.chores.complete;
-  return (
-    <ul>
-      {complete.map(chore => (
-        <li style={{ textDecoration: 'line-through', listStyle: 'none' }} key={`completeChore:${chore.chore_id}`} onClick={() => { props.undoComplete(chore.chore_id); }}>{chore.chore_name}</li>
-        ), this)
-      }
-    </ul>
-  );
+  if (complete) {
+    return (
+      <ul>
+        {complete.map(chore => (
+          <li style={{ textDecoration: 'line-through', listStyle: 'none' }} key={`completeChore:${chore.chore_id}`} onClick={() => { props.undoComplete(chore.chore_id); }}>{chore.chore_name}</li>
+          ), this)
+        }
+      </ul>
+    );
+  }
+  return null;
 }

cb284fdfb907ae0260c0d69322a0368b393295aa
Richard A. Serdici <rserdici@gmail.com>
2017-04-13 16:12:47 -0400

    feat(ui): Add Material Desigh

---
+++ src/config/menus.jsx
@@ -0,0 +1,6 @@
+menus: [
+  { text: 'DashBoard', icon: <Assessment/>, link: '/dashboard' },
+  { text: 'Form Page', icon: <Web/>, link: '/form' },
+  { text: 'Table Page', icon: <GridOn/>, link: '/table' },
+  { text: 'Login Page', icon: <PermIdentity/>, link: '/login' }
+],

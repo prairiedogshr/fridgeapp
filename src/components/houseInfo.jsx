483ef83e41ea8d616e95685e23284221a0044bc0
Richard A. Serdici <rserdici@gmail.com>
2017-04-14 13:14:46 -0400

    style(house feat): Lint

--- src/components/houseInfo.jsx
+++ src/components/houseInfo.jsx
@@ -1,48 +1,62 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { ControlLabel, FormControl, FormGroup, InputGroup } from 'react-bootstrap'
+import { ControlLabel, FormControl, FormGroup, InputGroup } from 'react-bootstrap';
 
 export default function HouseInfo({ info, update }) {
   return (
     <div>
       <form>
-        <FormGroup controlId="houseInfo" onBlur={(e) => {
-          console.log('hey!! ', e.target.id, ' ', e.target.value);
-          update({
-            key: e.target.id, 
-            value: e.target.value
-          })
-        }}>
-        <ControlLabel>Address</ControlLabel>
-        <FormControl  id="house_address" onClick={(e) => {}} type="text"
-        defaultValue={info.house_address} placeholder="Street Address"
-        />
-        <FormControl id="house_unit_number" type="text"
-        defaultValue={info.house_unit_number} placeholder = "Unit Number"
-        />
-        <FormControl id="house_city" type="text"
-        defaultValue={info.house_city} placeholder = "City"
-        />
-        <FormControl id="house_state" type="text"
-        defaultValue={info.house_state} placeholder = "State"
-        />
-        <FormControl id="house_zip" type="text"
-        defaultValue={info.house_zip} placeholder = "Zipcode"
-        />
-        <br/>
-        <ControlLabel>Info</ControlLabel>
-        <FormControl id="house_info" type="text"
-        defaultValue={info.house_info} placeholder = "House Info"
-        />
+        <FormGroup
+          controlId="houseInfo"
+          onBlur={(e) => {
+            console.log('hey!! ', e.target.id, ' ', e.target.value);
+            update({
+              key: e.target.id,
+              value: e.target.value,
+            });
+          }}
+        >
+          <ControlLabel>Address</ControlLabel>
+          <FormControl
+            id="house_address"
+            onClick={(e) => {}}
+            type="text"
+            defaultValue={info.house_address}
+            placeholder="Street Address"
+          />
+          <FormControl
+            id="house_unit_number"
+            type="text"
+            defaultValue={info.house_unit_number}
+            placeholder="Unit Number"
+          />
+          <FormControl
+            id="house_city"
+            type="text"
+            defaultValue={info.house_city}
+            placeholder="City"
+          />
+          <FormControl
+            id="house_state"
+            type="text"
+            defaultValue={info.house_state}
+            placeholder="State"
+          />
+          <FormControl
+            id="house_zip"
+            type="text"
+            defaultValue={info.house_zip}
+            placeholder="Zipcode"
+          />
+          <br />
+          <ControlLabel>Info</ControlLabel>
+          <FormControl
+            id="house_info"
+            type="text"
+            defaultValue={info.house_info}
+            placeholder="House Info"
+          />
         </FormGroup>
       </form>
     </div>
-  )
-};
-  
-
-
-
-
-
-
+  );
+}
--- src/containers/houseContainer.jsx
+++ src/containers/houseContainer.jsx
@@ -1,51 +1,53 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateHouseInfo, removeUser, addUser, getHouse } from '../actions/house/house.js';
-import HouseInfo from '../components/houseInfo.jsx';
 import { Col, Panel } from 'react-bootstrap';
-import Roommate from '../components/roommate.jsx';
+import { updateHouseInfo, removeUser, addUser, getHouse } from '../actions/house/house';
+import HouseInfo from '../components/houseInfo';
+import Roommate from '../components/roommate';
 
 class House extends Component {
 
-	componentWillMount() {
-		console.log('rendering with props: ', this.props)
-	}
+  componentWillMount() {
+    console.log('rendering with props: ', this.props);
+  }
 
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					<Col xs={6}>
-						<Panel header="HOUSE INFO">
-							<HouseInfo info={this.props.house} 
-							update={this.props.updateHouseInfo} />
-						</Panel>
-					</Col>
-					<Col xs={6}>
-						<Panel header="Roommates!">
-							{this.props.house.users.map(user => 
-								<Roommate roommate={user} />
-							)}
-						</Panel>
-				</Col>
-			</div>
-		</div>
-	)}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <Col xs={6}>
+            <Panel header="HOUSE INFO">
+              <HouseInfo
+                info={this.props.house}
+                update={this.props.updateHouseInfo}
+              />
+            </Panel>
+          </Col>
+          <Col xs={6}>
+            <Panel header="Roommates!">
+              {this.props.house.users.map(user =>
+                <Roommate roommate={user} />
+              )}
+            </Panel>
+          </Col>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = ({ houseReducer }) => ({
-	house: houseReducer,
-})
+  house: houseReducer,
+});
 
 export default connect(
-	mapStateToProps,
-	{
-		addUser,
-		removeUser,
-		updateHouseInfo,
-		getHouse
-	}
-	)(House)
+  mapStateToProps,
+  {
+    addUser,
+    removeUser,
+    updateHouseInfo,
+    getHouse,
+  },
+)(House);
 
 // export default connect(mapStateToProps, mapDispatchToProps)(House);

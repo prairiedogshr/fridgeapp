49a3a85078775446ecce4b2bc7a8dc4715184f22
Richard A. Serdici <rserdici@gmail.com>
2017-04-14 15:26:58 -0400

    fix(header): Lint

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -37,9 +37,6 @@ class Header extends Component {
       <div>
         <AppBar
           style={{ ...styles, ...style.appBar }}
-          // title={
-          //   <img src={logo} style={style.logo} alt="Fridge" />
-          // }
           iconElementLeft={
             <IconButton style={style.menuButton} onClick={handleChangeRequestNavDrawer}>
               <Menu color={white} />
@@ -47,18 +44,6 @@ class Header extends Component {
           }
           iconElementRight={
             <div style={style.iconsRightContainer}>
-              {/*<IconMenu*/}
-                {/*color={white}*/}
-                {/*iconButtonElement={*/}
-                  {/*<IconButton><ViewModule color={white} /></IconButton>*/}
-                {/*}*/}
-                {/*targetOrigin={{ horizontal: 'right', vertical: 'top' }}*/}
-                {/*anchorOrigin={{ horizontal: 'right', vertical: 'top' }}*/}
-              {/*>*/}
-                {/*<MenuItem key={1} primaryText="Application 1"/>*/}
-                {/*<MenuItem key={2} primaryText="Application 2"/>*/}
-                {/*<MenuItem key={3} primaryText="Application 3"/>*/}
-              {/*</IconMenu>*/}
               <IconMenu
                 color={white}
                 iconButtonElement={
--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -11,14 +11,9 @@ import Homeless from '../containers/homelessContainer';
 import House from '../containers/houseContainer';
 import HouseExpenses from '../containers/expensesContainer';
 import Join from '../containers/joinHouseContainer';
-import Log from '../components/log';
-import Login from '../components/login';
-import { logoutUser } from '../actions/auth/auth';
 import Profile from '../containers/profileContainer';
-import Register from '../components/register';
 import Settings from '../containers/settingsContainer';
 import Tasks from '../containers/tasksContainer';
-import User from '../components/user';
 
 class Routes extends Component {
   render() {
@@ -27,16 +22,15 @@ class Routes extends Component {
         <Switch>
           <Route path="/login" component={Auth} />
           <App>
-            <Route path="/profile" component={Profile} />
             <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/house" component={House} />
             <Route path="/chores" component={Chores} />
             <Route path="/tasks" component={Tasks} />
+            <Route path="/bills" component={HouseExpenses} />
             <Route path="/settings" component={Settings} />
-            <Route path="/house" component={House} />
             <Route path="/homeless" component={Homeless} />
             <Route path="/createhouse" component={CreateHouse} />
-            {/* <Route path="/joinhouse" component={JoinHouse} /> */}
-            <Route path="/bills" component={HouseExpenses} />
             <Route path="/join" component={Join} />
           </App>
         </Switch>

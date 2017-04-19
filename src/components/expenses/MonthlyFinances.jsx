3e6e650dd2a6c18efb694623421673e4e64183d3
dhpdx <danhendrix@gmail.com>
2017-04-19 12:24:18 -0400

    fix(expenses graph): add yearly look, add ability to see past months

--- src/components/expenses/MonthlyFinances.jsx
+++ src/components/expenses/MonthlyFinances.jsx
@@ -3,15 +3,23 @@ import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 
 export default function MonthlyFinances(props) {
+  const topExpenses = props.expenses.sort((a, b) => b.expense_balance - a.expense_balance)
+    .slice(0,5);
+  topExpenses.forEach((exp) => {
+    console.log(exp.expense_due)
+  })
   return (
     <div>
-    <List>
-      <ListItem primaryText="Monthly Total" secondaryText={props.expenses.reduce((all, item) => {
-        all += item.expense_balance
-        return all;
-      },0).toLocaleString()} />
-    </List>
-    <Divider inset={false} />
+      <List>
+        <ListItem primaryText="Your Monthly Expenses" secondaryText={props.expenses.reduce((all, item) => {
+          all += (item.expense_balance / props.roommates)
+          return all;
+        },0).toLocaleString()} />
+        <Divider inset={false} />
+        {topExpenses.map((exp) =>
+          <p>{exp.expense_name} - ${(exp.expense_balance/props.roommates).toLocaleString()}</p>
+        )}
+      </List>
     </div>
     )
 }

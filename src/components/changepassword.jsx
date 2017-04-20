a0d52b895acd82d5cdfe3dc34a0a11b448696b56
kylegreene <ksgreene1906@hotmail.com>
2017-04-20 18:22:11 -0400

    fixing some things

--- src/components/changepassword.jsx
+++ src/components/changepassword.jsx
@@ -22,8 +22,7 @@ class Change extends Component {
     this.state ={
       user_password: '',
       user_newpass: ''
-    }
-  }
+    };
 
 
   this.styles = {
@@ -41,13 +40,9 @@ class Change extends Component {
     loadRefresh: {
       display: 'inline-block',
       position: 'relative',
-    },
-  };
+      },
+    };
   }
-
-
-
-
 
 
 
@@ -56,40 +51,91 @@ class Change extends Component {
   };
 
   handleSubmit = (e) =>{
-    const old = this.refs.oldpass.value;
-    const new1 = this.refs.newpassword.value;
-    const new2 = this.refs.newpassword2.value;
+    event.preventDefault();
+
+    const old = this.id.oldpass.value;
+    const new1 = this.id.newpassword.value;
+    const new2 = this.id.newpassword2.value;
     const email = this.props.user.user_email;
     const user={"old": old, "new1": new1, "new2": new2, "email":email}
-    if( new1 !== new2){
-      alert("your new password fields dont match, try again");
-    }else{
-      this.props.changePassword(user)
-        .then((resp) => {
-          if(resp){
-            console.log("changed the password!");
-            this.props.history.push('/dashboard');
-          }else{
-            alert("Please enter the correct original password")
-          }
-        });
-    }
+    console.log(email);
+    console.log(new2);
+    // if( new1 !== new2){
+    //   alert("your new password fields dont match, try again");
+    // }else{
+    //   this.props.changePassword(user)
+    //     .then((resp) => {
+    //       if(resp){
+    //         console.log("changed the password!");
+    //         this.props.history.push('/dashboard');
+    //       }else{
+    //         alert("Please enter the correct original password")
+    //       }
+    //     });
+    // }
 
   };
 
+  // <div>
+  //   <input type='password' ref='oldpass' className="form-control" placeholder='oldpassword'/>
+  //   <input type='password' ref='newpassword' className="form-control" placeholder='newpassword'/>
+  //   <input type='password' ref='newpassword2' className="form-control" placeholder='confirm new password'/>
+  //   <button onClick={(event) => this.handleSubmit(event)} className="btn btn-primary">
+  //     change!
+  //   </button>
+  // </div>
+
+
+
   render(){
     return(
-      <div>
-        <input type='password' ref='oldpass' className="form-control" placeholder='oldpassword'/>
-        <input type='password' ref='newpassword' className="form-control" placeholder='newpassword'/>
-        <input type='password' ref='newpassword2' className="form-control" placeholder='confirm new password'/>
-        <button onClick={(event) => this.handleSubmit(event)} className="btn btn-primary">
-          change!
-        </button>
-      </div>
+      <MuiThemeProvider muiTheme={ThemeDefault}>
+        <Grid fluid>
+          <Row>
+            <Col xs={12}>
+              <Row center = "xs">
+                <Col md ={4}>
+                  <Paper style = {this.styles.paper}>
+                    <h2>Change password</h2>
+                    <form onSubmit={event => this.handleSubmit(event)}>
+                      <TextField
+                        id="oldpass"
+                        floatingLabelText = "Old Password"
+                        fullWidth={true}
+
+                        type = "password"
+                      />
+                      <TextField
+                        id = "newpassword"
+                        floatingLabelText = "New Password"
+                        fullWidth={true}
+
+                        type = "password"
+                      />
+                      <TextField
+                        id="newpassword2"
+                        floatingLabelText = "New Password"
+                        fullWidth={true}
+                        type = "password"
+                      />
+                      <RaisedButton
+                        label="Login"
+                        primary={true}
+                        style={{...this.styles.loginBtn, ...this.styles.btnSpan}}
+                        type="submit"
+                      />
+                    </form>
+                  </Paper>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     )
   }
 }
+
 
 Change.propTypes = {
   changePassword: React.PropTypes.func,
